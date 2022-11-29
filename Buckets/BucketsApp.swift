33a014a2e27f991fab9d149d10a03176de31cc8a
selimdogan <selim-dgn@hotmail.com>
2022-11-29 18:13:30 +0300

    Initial Commit

---
+++ Buckets/BucketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketsApp.swift
+//  Buckets
+//
+//  Created by Selim Doğan on 29.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
